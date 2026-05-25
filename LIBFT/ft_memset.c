fd397f27a57e69ed96c377ff8878e8b56fbfc743
Henrique Frameschi <henriqueframeschi@Thais.local>
2026-05-25 20:45:11 -0300

    bzero, memset

---
+++ LIBFT/ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henriqueframeschi <henriqueframeschi@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 19:59:52 by henriquefra       #+#    #+#             */
+/*   Updated: 2026/05/25 20:42:26 by henriquefra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *memset(void *s, int c, size_t n)
+{
+    unsigned char   *temp;
+    size_t i;
+
+    i = 0;
+    temp = (unsigned char *)s;
+    if (n == 0)
+        return (s);
+    while (i < n)
+    {
+        temp[i] = (unsigned char)c;
+        i++;
+    }
+    return (s);
+}
