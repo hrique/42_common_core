4ee3e43c189187c051ded0e39eb07fb946cc3308
hrique <hrique.frameschi@gmail.com>
2026-05-25 23:27:57 -0300

    makefile e lib

---
+++ LIBFT/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrique <hrique@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/22 15:43:00 by hrique            #+#    #+#             */
+/*   Updated: 2026/05/22 17:33:30 by hrique           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* se a macro LIBFT_H não foi definida anteriormente */
+#ifndef LIBFT_H
+
+/* defina LIBFT_H */
+# define LIBFT_H
+
+/* bibliotecas inclusas, sempre dar espaço após o # */
+# include <stdlib.h>
+# include <unistd.h>
+
+/* \/ aqui vem os prototipos das funções */
+
+/* encerra o bloco condicional */
+#endif
