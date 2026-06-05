78309a233b8e5dae99c9768c3cf4a4e19f191267
henriqueframeschi <66852208+hrique@users.noreply.github.com>
2026-06-05 18:02:00 -0300

    Add files via upload
    socorro

--- LIBFT/ft_split.c
+++ LIBFT/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hesantan <hesantan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 14:58:06 by hesantan          #+#    #+#             */
-/*   Updated: 2026/06/03 17:45:46 by hesantan         ###   ########.fr       */
+/*   Updated: 2026/06/05 17:59:48 by hesantan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,44 @@ static size_t	ct_words(char const *s, char c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		while (s[i] == c && s[i] != '\0')
-			i++;
-		if (s[i] != '\0')
+		if (s[i] == c)
 		{
-			words++;
-			while (s[i] != '\0' && s[i] != c)
+			while (s[i] == c && s[i] != '\0')
 				i++;
 		}
+		if (s[i] != '\0')
+			words++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
 	}
 	return (words);
 }
 
-static size_t	ct_letters(char const *s, char c)
+static size_t	ct_size(char const *s, char c, size_t start)
 {
-	size_t	start;
-	size_t	end;
-	size_t	i;
+	size_t	size;
 
-	i = 0;
-	while (s[i] != '\0')
+	size = 0;
+	if (s[start] != '\0')
 	{
-		while (s[i] == c && s[i] != '\0')
-			i++;
-		if (s[i] != '\0')
+		while (s[start] != c && s[start] != '\0')
 		{
-			start = i;
-			while (s[i] != '\0' && s[i] != c)
-				i++;
-			end = i;
+			size++;
+			start++;
 		}
-		return (end - start);
 	}
+	return (size);
+}
+
+static char	*free_list(char *list, size_t index)
+{
+	while (index > 0)
+	{
+		index--;
+		free(list[index]);
+	}
+	free(list);
+	return (list);
 }
 
 char	**ft_split(char const *s, char c)
@@ -60,16 +66,40 @@ char	**ft_split(char const *s, char c)
 	size_t	words;
 	size_t	size;
 	size_t	i;
-	char	**ptr;
+	size_t	j;
+	size_t	start;
+	char	**list;
 
-	i = 0;
 	words = ct_words(s, c);
-	ptr = malloc((words + 1) * sizeof(char *));
-	if (ptr == NULL)
+	list = malloc((words + 1) * sizeof(char *));
+	if (list == NULL)
 		return (NULL);
+	i = 0;
+	j = 0;
 	while (i < words)
 	{
-		
+		start = 0;
+		if (s[j] == c)
+		{
+			while (s[j] == c)
+				j++;
+		}	
+		size = ct_size(s, c, j);
+		list[i] = malloc((size + 1) * sizeof(char));
+		if (list[i] == NULL)
+			free_list(list, i);
+		start = 0;
+		while (start < size)
+		{
+			list[i][start] = s[j];
+			j++;
+			start++;
+		}
+		list[i][start] = '\0';
+		i++;
+		j = size + j;
 	}
-
+}
+	list[i] = NULL;
+	return (list);
 }
