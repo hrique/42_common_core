fa19efa048929374bc3c0b5b39fac812902de00b
hrique <hrique.frameschi@gmail.com>
2026-06-09 17:17:54 -0300

    corrigido strlcat

--- LIBFT/ft_strlcat.c
+++ LIBFT/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hesantan <hesantan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hrique <hrique@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 15:11:35 by hesantan          #+#    #+#             */
-/*   Updated: 2026/05/28 15:56:49 by hesantan         ###   ########.fr       */
+/*   Updated: 2026/06/09 17:13:11 by hrique           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	ct_dst;
 	size_t	ct_src;
 
-	ct_dst = ft_strlen(dst);
+	ct_dst = 0;
+	while (ct_dst < size && dst[ct_dst] != '\0')
+		ct_dst++;
 	ct_src = ft_strlen(src);
 	if (size <= ct_dst)
 		return (size + ct_src);
