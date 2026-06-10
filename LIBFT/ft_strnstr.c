125125970405a6edabe590af24817d0dfb11145d
hrique <hrique.frameschi@gmail.com>
2026-06-10 10:12:57 -0300

    strnstr corrigida

--- LIBFT/ft_strnstr.c
+++ LIBFT/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hrique <hrique@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 20:55:38 by hrique            #+#    #+#             */
-/*   Updated: 2026/06/09 23:17:53 by hrique           ###   ########.fr       */
+/*   Updated: 2026/06/10 10:10:19 by hrique           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,12 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (t_big[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (little[j] != '\0' && t_big[i + j] == little[j] && (i + j < len))
+		while (little[j] != '\0' && t_big[i + j] == little[j])
+		{
+			if ((i + j) >= len)
+				break ;
 			j++;
+		}
 		if (little[j] == '\0')
 			return (&t_big[i]);
 		i++;
